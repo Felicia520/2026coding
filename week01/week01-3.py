632c79604c6e9315d93722cf0194a69b3a871ce3
Felicia520 <welovefelicia@gmail.com>
2026-02-26 16:23:16 +0800

    week01

---
+++ week01/week01-3.py
@@ -0,0 +1,13 @@
+# week01-3.py
+#LeetCode 1071. Greatest Common Divisor of Strings
+#程そ计gcd ﹃( ぃ璶組賂盾?)
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        #蛤程そ计gcdΤ闽
+        N1, N2 = len(str1), len(str2) # ㄢ﹃
+        N = gcd(N1, N2) # 程そ计
+        ans = str1[:N] #﹃1玡Nダ
+
+        if ans*(N1//N) != str1: return "" #ぃ才, 碞ア毖
+        if ans*(N2//N) != str2: return ""
+        return str1[:N]
